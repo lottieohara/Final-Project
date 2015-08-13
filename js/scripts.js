c56317708f2da75b8946c4ce586e25af5ba8dade
lottieohara <charlotte.e.ohara@gmail.com>
2015-08-12 18:54:50 -0700

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+
+// $('body').on('click', function() {
+// 	$('body').css({
+// 	'backgroundColor': 'red'
+
+// 	});
+// });
+
+// $('.test-class').on('click', function() {
+// 	$('.test-class').css({
+// 		'fontSize' : '30px'
+// 	});
+
+// });
+
+// $('body').on('shaneTestEvent', function() {
+// 	alert('test');
+
+// });
+
+// $('img').on('click', function() {
+// 	$('body').trigger('shaneTestEvent');
+// });
+
+$('.img-circle').on('show.bs.modal', function() {
+	$('.big-image').attr('src', 'http://cache4.asset-cache.net/xc/532415641.jpg?v=2&c=IWSAsset&k=2&d=dqxR9dcuALWOuHNfqpwCHYGYxvoGYgRZvs_t7uqNR1etm6Ciy-SdjvYgXD2-kTMm0');
+})
